126eee7983b1e9e628712bc3762b705065e55355
Leandro Luiz <leandro.wdsn@gmail.com>
2026-08-25 08:56:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -765,12 +765,12 @@ document.getElementById('formReservas').addEventListener('submit', (e) => {
                     const nomeStr = m.nome.toLowerCase();
 
                 if (termoBusca.length === 1 && /^[a-z]$/.test(termoBusca)) {
-    return nomeStr.split(' ').some(palavra => palavra.startsWith(termoBusca));
-}
+                    return nomeStr.split(' ').some(palavra => palavra.startsWith(termoBusca));
 
-return casaStr === termoBusca || 
-       casaStr.startsWith(termoBusca + ' ') || 
-       nomeStr.includes(termoBusca);
+
+                return casaStr === termoBusca || 
+               casaStr.startsWith(termoBusca + ' ') || 
+               nomeStr.includes(termoBusca);
                 if (moradoresFiltrados.length === 0) {
                     lista.innerHTML = `<div class="p-4 text-center text-gray-400 italic">Nenhum morador encontrado.</div>`;
                     return;
